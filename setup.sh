aed66bd39bc85ab5467cd934672f44c680924d51
muskan tandel <muskantandel824@gmail.com>
2026-01-19 16:40:15 +0000

    update .sh file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+#mkdir used for make directory
 mkdir -p project/{docs,scripts,logs}
+#touch for create a files(.txt, .sh, .log, .sh)
 touch project/docs/readme.txt project/scripts/run.sh project/logs/app.log
 chmod 644 project/docs/readme.txt
 chmod 755 project/scripts/run.sh
